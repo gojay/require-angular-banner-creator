7c0b73c6b730bb85bec6e7d5b3da2101a8785a22
h4rri577@gmail.com <luffyg0mun0>
2013-08-27 11:32:43 +0700

    add api init errors

--- api/index.php
+++ api/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors','on');
 include '../config.php';
 // Autoload
 include 'vendor/autoload.php';
--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
+
 // PATH
 define('BASE_PATH', dirname(__FILE__));
 define('UPLOAD_PATH', BASE_PATH . '/images/upload');
 // URL
-define('ABS_URL', 'http://dev.angularjs/_learn_/require-angular-banner-creator');
+define('ABS_URL', 'bannercreator/');
 // define('UPLOAD_URL', ABS_URL . '/images/upload');
 define('UPLOAD_URL', 'images/upload');
 // DB Config
 $db_config = array(
 	'host'     => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'dbname'   => 'ch',
 );
